b9b24ed96d423f11e16ddd7b848ab02aef8ad917
Timothy Morton <wookie-hunter@hotmail.com>
2022-03-07 21:15:04 +1300

    added maybe class

---
+++ AvaloniaMVVMTest/Models/Maybe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaMVVMTest.Models
+{
+    public class Maybe<T> where T : class
+    {
+        private readonly T? value;
+
+        public Maybe(T someValue)
+        {
+            if (someValue == null)
+                throw new ArgumentNullException(nameof(someValue));
+            this.value = someValue;
+        }
+
+        private Maybe(){}
+
+        public Maybe<U> Bind<U>(Func<T, Maybe<U>> func) where U : class
+        {
+            return value != null ? func(value) : Maybe<U>.None();
+        }
+
+        public static Maybe<T> None() => new();
+    }
+}
